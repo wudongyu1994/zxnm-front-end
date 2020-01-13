a71d544bc6dc65d7a57a60455f316b8fad6926bf
wdy-615-lenovo <wudongyu1994@foxmail.com>
2020-01-13 09:26:06 +0800

    cmt0113: order page basic ok

---
+++ src/api/order.js
@@ -0,0 +1,29 @@
+import request from '@/utils/request'
+
+export function getOrderByPage(params) {
+  return request({
+    url: '/order',
+    method: 'get',
+    params
+  })
+}
+export function addOrder(data) {
+  return request({
+    url: '/order',
+    method: 'post',
+    data
+  })
+}
+export function modifyOrder(data) {
+  return request({
+    url: '/order/' + data.id,
+    method: 'patch',
+    data
+  })
+}
+export function deleteOrder(id) {
+  return request({
+    url: '/order/' + id,
+    method: 'delete'
+  })
+}
--- src/views/order/index.vue
+++ src/views/order/index.vue
@@ -5,40 +5,87 @@
       <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
         Search
       </el-button>
-      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-plus" @click="handleCreate">
-        Add
-      </el-button>
+      <!--      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-plus" @click="handleCreate">-->
+      <!--        Add-->
+      <!--      </el-button>-->
     </div>
 
     <el-table
       :key="tableKey"
       v-loading="listLoading"
-      :data="roleListRaw"
+      :data="orderListRaw"
       border
       fit
       highlight-current-row
       style="width: 100%;"
     >
-      <el-table-column label="ID" prop="id" align="center" width="50">
-        <template slot-scope="{row: {id}}">
-          <span>{{ id }}</span>
+      <el-table-column label="orderNo" prop="orderNo" align="center" width="80">
+        <template slot-scope="{row: {orderNo}}">
+          <span>{{ orderNo }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Name" align="center" width="200">
-        <template slot-scope="{row: {name}}">
-          <span>{{ name }}</span>
+      <el-table-column label="userId" align="center" width="50">
+        <template slot-scope="{row: {userId}}">
+          <span>{{ userId }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Description" width="400">
-        <template slot-scope="{row: {description}}">
-          <span>{{ description }}</span>
+      <el-table-column label="expressNo" width="80">
+        <template slot-scope="{row: {expressNo}}">
+          <span>{{ expressNo }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Actions" align="center" width="200" class-name="small-padding fixed-width">
+      <el-table-column label="money" width="80">
+        <template slot-scope="{row: {money}}">
+          <span>{{ money }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="orderStatus" width="100">
+        <template slot-scope="{row: {orderStatus}}">
+          <el-tag :type="statusEnum[orderStatus] | statusFilter">
+            {{ statusEnum[orderStatus] }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="payTime" width="80">
+        <template slot-scope="{row: {payTime}}">
+          <span>{{ payTime }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="deliveryTime" width="80">
+        <template slot-scope="{row: {deliveryTime}}">
+          <span>{{ deliveryTime }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="receiveTime" width="80">
+        <template slot-scope="{row: {receiveTime}}">
+          <span>{{ receiveTime }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="address" width="150">
+        <template slot-scope="{row: {address}}">
+          <span>{{ address }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="phone" width="120">
+        <template slot-scope="{row: {phone}}">
+          <span>{{ phone }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="note" width="150">
+        <template slot-scope="{row: {note}}">
+          <span>{{ note }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="Actions" align="center" width="100" class-name="small-padding fixed-width">
         <template slot-scope="{row}">
+          <el-button type="success" size="mini" icon="el-icon-order" @click="handleShowGoods(row)">
+            Goods
+          </el-button>
+          <br>
           <el-button type="primary" size="mini" icon="el-icon-edit" @click="handleUpdate(row)">
             Edit
           </el-button>
+          <br>
           <el-button size="mini" type="danger" icon="el-icon-delete" @click="handleDelete(row)">
             Delete
           </el-button>
@@ -47,24 +94,49 @@
 
     </el-table>
 
-    <pagination v-show="total>0" :total="total" :page.sync="pageSetting.page" :limit.sync="pageSetting.size" @pagination="getRole" />
+    <pagination v-show="total>0" :total="total" :page.sync="pageSetting.page" :limit.sync="pageSetting.size" @pagination="getOrder" />
 
     <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
       <el-form ref="dataForm" :rules="rules" :model="temp" label-position="right" label-width="100px" style="width: 400px; margin-left:50px;">
-        <el-form-item label="Id" prop="id">
+        <el-form-item label="id" prop="id">
           <span>{{ temp.id }}</span>
         </el-form-item>
-        <el-form-item label="Name" prop="name">
-          <el-input v-model="temp.name" />
+        <el-form-item label="orderNo" prop="orderNo">
+          <span>{{ temp.orderNo }}</span>
         </el-form-item>
-        <el-form-item label="Description" prop="description">
-          <el-input v-model="temp.description" type="textarea" :rows="3" />
+        <el-form-item label="userId" prop="userId">
+          <span>{{ temp.userId }}</span>
         </el-form-item>
-        <el-form-item label="Permission" prop="permission">
-          <el-tree ref="tree" :data="listPermissionTree" show-checkbox default-expand-all node-key="id" highlight-current :props="myProps" @check-change="handlePerChange" />
+        <el-form-item label="expressNo" prop="expressNo">
+          <span>{{ temp.expressNo }}</span>
+        </el-form-item>
+        <el-form-item label="money" prop="money">
+          <span>{{ temp.money }}</span>
+        </el-form-item>
+        <el-form-item label="orderStatus" prop="orderStatus">
+          <el-tag :type="statusEnum[temp.orderStatus] | statusFilter">
+            {{ statusEnum[temp.orderStatus] }}
+          </el-tag>
+        </el-form-item>
+        <el-form-item label="payTime" prop="payTime">
+          <span>{{ temp.payTime }}</span>
+        </el-form-item>
+        <el-form-item label="deliveryTime" prop="deliveryTime">
+          <span>{{ temp.deliveryTime }}</span>
+        </el-form-item>
+        <el-form-item label="receiveTime" prop="receiveTime">
+          <span>{{ temp.receiveTime }}</span>
+        </el-form-item>
+        <el-form-item label="address" prop="address">
+          <el-input v-model="temp.address" />
+        </el-form-item>
+        <el-form-item label="phone" prop="phone">
+          <el-input v-model="temp.phone" />
+        </el-form-item>
+        <el-form-item label="note" prop="note">
+          <el-input v-model="temp.note" />
         </el-form-item>
       </el-form>
-
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">
           Cancel
@@ -78,18 +150,21 @@
 </template>
 
 <script>
+import { getOrderByPage, modifyOrder, deleteOrder } from '../../api/order'
 import Pagination from '../../components/Pagination'
 import { MessageBox } from 'element-ui'
 
 export default {
-  name: 'Role',
+  name: 'Order',
   components: { Pagination },
   filters: {
     statusFilter(status) {
       const statusMap = {
-        disable: 'info',
-        valid: 'success',
-        locked: 'info'
+        CREATED: 'danger', // red
+        PAID: 'warning', // yellow
+        PRODUCE: 'success', // green
+        DELIVERY: '', // blue
+        RECEIVED: 'info' // grey
       }
       return statusMap[status]
     }
@@ -97,34 +172,20 @@ export default {
   data() {
     return {
       statusEnum: [
-        'disable',
-        'valid',
-        'locked'
-      ],
-      sexEnum: [
-        'F',
-        'M'
+        'CREATED',
+        'PAID',
+        'PRODUCE',
+        'DELIVERY',
+        'RECEIVED'
       ],
       textMap: {
         update: 'Edit',
         create: 'Create'
       },
-      myProps: {
-        label: 'name',
-        children: 'children'
-      },
       tableKey: 0,
-      roleListRaw: [],
-      listPermissionTree: [],
-      listPermissionKey: [],
-      props: {
-        label: 'label',
-        children: 'children'
-      },
-      listCheck: [],
+      orderListRaw: [],
       total: 0,
       listLoading: true,
-      perLoading: true,
       pageSetting: {
         page: 1,
         size: 10
@@ -134,41 +195,37 @@ export default {
         createTime: new Date(),
         updateTime: new Date(),
         name: '',
-        description: '',
-        permissionList: [],
-        listPermissionId: []
+        orderNo: '',
+        userId: 1,
+        orderStatus: 0,
+        payTime: new Date(),
+        deliveryTime: new Date(),
+        receiveTime: new Date(),
+        money: 0,
+        address: '',
+        phone: '',
+        note: '',
+        expressNo: ''
       },
       dialogFormVisible: false,
-      dialogStatus: '',
-      rules: {
-        name: { required: true, message: 'name is required', trigger: 'blur' }
-      }
+      dialogStatus: ''
     }
   },
   created() {
-    this.getRole()
-    this.getTree()
+    this.getOrder()
   },
   methods: {
-    getRole() {
-      // this.listLoading = true
-      // getRoleByPage(this.pageSetting).then(response => {
-      //   this.roleListRaw = response.data
-      //   this.total = response.count
-      //   this.listLoading = false
-      // })
-    },
-    getTree() {
-      // this.listLoading = true
-      // getAllPermissionTree().then(response => {
-      //   this.listPermissionTree = response.data
-      //   this.total = response.count
-      //   this.listLoading = false
-      // })
+    getOrder() {
+      this.listLoading = true
+      getOrderByPage(this.pageSetting).then(response => {
+        this.orderListRaw = response.data
+        this.total = response.count
+        this.listLoading = false
+      })
     },
     handleFilter() {
       this.pageSetting.page = 1
-      this.getRole()
+      this.getOrder()
     },
     resetTemp() {
       this.temp = {
@@ -176,22 +233,29 @@ export default {
         createTime: new Date(),
         updateTime: new Date(),
         name: '',
-        description: '',
-        permissionList: [],
-        listPermissionId: []
+        orderNo: '',
+        userId: 1,
+        orderStatus: 0,
+        payTime: new Date(),
+        deliveryTime: new Date(),
+        receiveTime: new Date(),
+        money: 0,
+        address: '',
+        phone: '',
+        note: '',
+        expressNo: ''
       }
     },
-    handlePerChange() {
-      // console.log(this.$refs.tree.getCheckedKeys().concat(this.$refs.tree.getHalfCheckedKeys()))
-    },
-    handleCreate() {
-      this.resetTemp()
-      this.dialogStatus = 'create'
-      this.dialogFormVisible = true
-      this.$nextTick(() => {
-        this.$refs['dataForm'].clearValidate()
-        this.$refs.tree.setCheckedKeys([])
-      })
+    // handleCreate() {
+    //   this.resetTemp()
+    //   this.dialogStatus = 'create'
+    //   this.dialogFormVisible = true
+    //   this.$nextTick(() => {
+    //     this.$refs['dataForm'].clearValidate()
+    //   })
+    // },
+    handleShowGoods(row) {
+
     },
     handleUpdate(row) {
       this.temp = Object.assign({}, row) // copy obj
@@ -199,7 +263,6 @@ export default {
       this.dialogFormVisible = true
       this.$nextTick(() => {
         this.$refs['dataForm'].clearValidate()
-        this.$refs.tree.setCheckedKeys(this.temp.permissionList.map(per => per.id))
       })
     },
     handleDelete(row) {
@@ -208,38 +271,36 @@ export default {
         cancelButtonText: 'Cancel',
         type: 'error'
       }).then(() => {
-        // this.temp = Object.assign({}, row) // copy obj
-        // deleteRole(this.temp.id).then(() => {
-        //   this.getRole()
-        //   this.$message.success('Delete successfully!')
-        // })
+        this.temp = Object.assign({}, row) // copy obj
+        deleteOrder(this.temp.id).then(() => {
+          this.getOrder()
+          this.$message.success('Delete successfully!')
+        })
       })
     },
-    createData() {
-      this.$refs['dataForm'].validate((valid) => {
-        if (valid) {
-          this.temp.createTime = new Date().getTime()
-          this.temp.updateTime = this.temp.createTime
-          this.temp.listPermissionId = this.$refs.tree.getCheckedKeys()
-          // addRole(this.temp).then(() => {
-          //   this.dialogFormVisible = false
-          //   this.$message.success('Create successfully!')
-          //   this.getRole()
-          // })
-        }
-      })
-    },
+    // createData() {
+    //   this.$refs['dataForm'].validate((valid) => {
+    //     if (valid) {
+    //       this.temp.createTime = new Date().getTime()
+    //       this.temp.updateTime = this.temp.createTime
+    //       // addOrder(this.temp).then(() => {
+    //       //   this.dialogFormVisible = false
+    //       //   this.$message.success('Create successfully!')
+    //       //   this.getOrder()
+    //       // })
+    //     }
+    //   })
+    // },
     updateData() {
       this.$refs['dataForm'].validate((valid) => {
         if (valid) {
           this.temp.updateTime = new Date().getTime()
-          this.temp.listPermissionId = this.$refs.tree.getCheckedKeys()
           const tempData = Object.assign({}, this.temp)
-          // modifyRole(tempData).then(() => {
-          //   this.dialogFormVisible = false
-          //   this.$message.success('Update successfully!')
-          //   this.getRole()
-          // })
+          modifyOrder(tempData).then(() => {
+            this.dialogFormVisible = false
+            this.$message.success('Update successfully!')
+            this.getOrder()
+          })
         }
       })
     }
